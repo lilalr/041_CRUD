149d85315590109c7fbacf86e510224a87e47bd2
lilalr <gurennut@gmail.com>
2025-10-20 12:35:54 +0700

    menambah pengecekan connection error handling

--- index.js
+++ index.js
@@ -22,3 +22,11 @@ const db = mysql.createConnection({
   port: 3308 
 })
 
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database:' + err.stack);
+    return;
+  }
+  console.log('Connection succesfully');
+})
+
